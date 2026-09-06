0e710b46978a75841484d4929c1eb0a797e436c2
lynnhunki-source <lynnhunki@gmail.com>
2026-09-06 20:07:00 +0300

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -14,7 +14,7 @@ export const products: Product[] = [
     id: "orange-pi-5-plus",
     name: "Orange Pi 5 Plus 16GB",
     description: "حاسبة لوحية قوية بذاكرة 16GB ومعالج ثماني النواة لمشاريع الذكاء الاصطناعي والأنظمة المدمجة.",
-    price: 155000,
+    price: 270000,
     image: "/products/orange-pi-5-plus.png",
     category: "لوحات",
   },
